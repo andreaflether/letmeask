9c98e0c37e17cee80f5766517ed78c79728203b1
Andréa Alencar <andreaflether@gmail.com>
2021-06-23 21:32:56 -0300

    Add links and Firebase Google auth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,7 @@
+import { useHistory } from 'react-router-dom'
+
+import { auth, firebase } from '../services/firebase'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -5,7 +9,19 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import '../styles/auth.scss'
 import { Button } from '../components/Button'
 
+
 export function Home() {
+  const history = useHistory()
+
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result)
+    })
+
+    // history.push('/rooms/new')
+  }
+
   return(
     <div id="page-auth">
       <aside>
@@ -17,7 +33,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Logo da Letmeask" />
-          <Button className="button create-room">
+          <Button className="button create-room" onClick={handleCreateRoom}>
             <img src={googleIconImg} alt="Logo do Google"/>
             Crie sua sala com o Google
           </Button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 
@@ -24,7 +26,7 @@ export function NewRoom() {
             />
             <Button className="button primary" type="submit">Criar sala</Button>
           </form>
-          <p>Quer entrar em uma sala já existente? <a href="#">Clique aqui</a></p>
+          <p>Quer entrar em uma sala já existente? <Link to="/">Clique aqui</Link></p>
         </div>
       </main>
     </div>
